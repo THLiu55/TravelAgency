e71103ec2413a1106750334755a84bbb98618382
Qi Sun <2769059069@qq.com>
2023-03-11 10:49:18 +0800

    update staff topbar

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template("ProductsDetail.html")
+    # return render_template("ProductsDetail.html")
+    return render_template("StaffMainPage.html")
 
 
 if __name__ == '__main__':
