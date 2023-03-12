1029843f1e77eb16b445e065514aaa3fe589943c
Gzq <ziqi.guo@ucdconnect.ie>
2023-03-12 14:35:11 +0800

    hash

--- generate_hash.py
+++ generate_hash.py
@@ -8,15 +8,15 @@ def hash_text(text):
 
 def get_hash_time(email):
     now = datetime.datetime.now()
-    email = email + str(now.year) + str(now.month) + str(now.hour)
+    email = email + str(now.year) + str(now.month) + str(now.day) + str(now.hour)
     return hash_text(email)
 
 
 def check_hash_time(email, captcha):
     now = datetime.datetime.now()
-    hash_value1 = hash_text(email + str(now.year) + str(now.month) + str(now.hour))
-    if now.hour == 23:
-        hash_value2 = hash_text(email + str(now.year) + str(now.month) + str('1'))
+    hash_value1 = hash_text(email + str(now.year) + str(now.month) + str(now.day) + str(now.hour))
+    if now.hour == 0:
+        hash_value2 = hash_text(email + str(now.year) + str(now.month) + str(now.day-1) + str(23))
     else:
-        hash_value2 = hash_text(email + str(now.year) + str(now.month) + str(now.hour-1))
+        hash_value2 = hash_text(email + str(now.year) + str(now.month) + str(now.day) + str(now.hour-1))
     return hash_value1 == captcha or hash_value2 == captcha
