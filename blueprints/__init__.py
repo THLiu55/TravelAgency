0431c0096025bd6feaa910c2fb9125a70433d0b3
Gzq <ziqi.guo@ucdconnect.ie>
2023-03-20 20:58:10 +0800

    register

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -1,5 +1,7 @@
 from .customer import bp as customer_bp
+from .activity import bp as activity_bp
 
 
 def bp_register_all(app):
     app.register_blueprint(customer_bp)
+    app.register_blueprint(activity_bp)
