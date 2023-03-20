999724c08ec976d9dee69fbf4792d0292316b247
Zexuan Lyu <20205790@ucdconnect.ie>
2023-03-20 21:10:01 +0800

    add socketio to exts

--- exts.py
+++ exts.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_babel import Babel, gettext as _, refresh
+from flask_socketio import SocketIO, emit
 
 from utils.toys import get_locale
 
@@ -9,9 +10,7 @@ db = SQLAlchemy()
 mail = Mail()
 migrate = Migrate()
 babel = Babel()
-
-
-
+socketio = SocketIO()
 
 
 def exts_load_all(app):
@@ -19,3 +18,4 @@ def exts_load_all(app):
     mail.init_app(app)
     migrate.init_app(app, db)
     babel.init_app(app, locale_selector=get_locale)
+    socketio.init_app(app, cors_allowed_origins="*")
