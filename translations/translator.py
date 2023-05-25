416f6d7aca6b3329829f1849f89f4534feeaae05
Tianhao Liu <liutianhao328@gmail.com>
2023-05-25 17:21:18 +0800

    token 动态生成

--- translations/translator.py
+++ translations/translator.py
@@ -2,25 +2,21 @@ import requests
 import random
 import json
 
-
-API_KEY = "w22DiAAmDvd1aUotmZCCTFjY"
-SECRET_KEY = "cmdgasS2vG4wgcpXyv00wGCtG10GrRad"
-
-
-def get_access_token():
-    """
-    使用 AK，SK 生成鉴权签名（Access Token）
-    :return: access_token，或是None(如果错误)
-    """
-    url = "https://aip.baidubce.com/oauth/2.0/token"
-    params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
-    return str(requests.post(url, params=params).json().get("access_token"))
-
-
-token=get_access_token()
-
-# q: sentence in original language; from_lang: current language; dst_lang:  target language; # Return:translated sentences
 def translator(q, from_lang, to_lang):
+    # update token
+    API_KEY = "w22DiAAmDvd1aUotmZCCTFjY"
+    SECRET_KEY = "cmdgasS2vG4wgcpXyv00wGCtG10GrRad"
+    def get_access_token():
+        """
+        使用 AK，SK 生成鉴权签名（Access Token）
+        :return: access_token，或是None(如果错误)
+        """
+        url = "https://aip.baidubce.com/oauth/2.0/token"
+        params = {"grant_type": "client_credentials", "client_id": API_KEY, "client_secret": SECRET_KEY}
+        return str(requests.post(url, params=params).json().get("access_token"))
+    token = get_access_token()
+
+
     if q == "":
         return ""
     url = 'https://aip.baidubce.com/rpc/2.0/mt/texttrans/v1?access_token=' + token
